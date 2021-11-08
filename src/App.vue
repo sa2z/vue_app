eb0f13a9510775d0c74ecc08a3a828d2f3a0f1ed
sa2z <57865727+sa2z@users.noreply.github.com>
2021-11-09 00:05:47 +0900

    vue_test

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" width="25%">
-    <HelloWorld msg="Hello Vue in CodeSandbox!"/>
+    <side-bar></side-bar>
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
+    <!-- <b-button v-b-toggle.sidebar-1>Toggle Sidebar</b-button>
+    <b-sidebar id="sidebar-1" title="Sidebar" shadow>
+      <div class="px-3 py-2">
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+        <b-img
+          src="https://picsum.photos/500/500/?image=54"
+          fluid
+          thumbnail
+        ></b-img>
+      </div>
+    </b-sidebar> -->
   </div>
 </template>
 
 <script>
 import HelloWorld from "./components/HelloWorld";
-
+import SideBar from "./views/SideBar.vue";
 export default {
   name: "App",
   components: {
-    HelloWorld
-  }
+    SideBar,
+  },
 };
 </script>
 
